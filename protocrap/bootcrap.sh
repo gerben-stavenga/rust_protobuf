49da09a43f7f87cb35db05d23e4c74cefc4174d8
gerben-stavenga <stavenga@gmail.com>
2025-12-20 04:35:42 -0800

    bootstrapped

---
+++ protocrap/bootcrap.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -euo pipefail
+cd "$(dirname "$0")/.."
+# Build the codegen tool, to prevent rebuild when descriptor set is piped
+cargo build -p protocrap-codegen --bin protocrap-codegen
+# Generate the descriptor set
+protoc --include_imports --descriptor_set_out=/tmp/protocrap-descriptor-set.bin protocrap/proto/descriptor.proto
+RUST_BACKTRACE=full cargo run -p protocrap-codegen --bin protocrap-codegen -- /tmp/protocrap-descriptor-set.bin protocrap/src/descriptor.pc.rs
